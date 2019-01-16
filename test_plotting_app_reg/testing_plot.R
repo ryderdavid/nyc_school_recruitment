e9cb1f6b58e3335666e742be32c08907b24585e9
Ryder Cobean <ryderdavid@gmail.com>
2019-01-15 21:28:17 -0500

    Update testing_plot.R

--- test_plotting_app_reg/testing_plot.R
+++ test_plotting_app_reg/testing_plot.R
@@ -30,6 +30,7 @@ set_sourcefile_wd()
 
 
 example <- read_csv("example.csv")
+example$year <- as.factor(example$year)
 
 plot_conversions_by_sd <- function(year = seq(1950,2050), palette = "Greys", data = example) {
   
@@ -37,17 +38,21 @@ plot_conversions_by_sd <- function(year = seq(1950,2050), palette = "Greys", dat
   p <-  palette
   
   # This plots color of each 
-  ggplot(sd_stats_top, aes(x = as.factor(SchoolDist), y = count)) + 
-    geom_bar(stat = "identity", fill = "grey55") + coord_flip() +
+  ggplot(data, aes(x = as.factor(SchoolDist), y = count)) + 
+    geom_bar(stat = "identity", fill = "grey55") + 
+    theme(axis.title.x = element_blank()) + 
+    coord_flip() +
     # scale_color_brewer(palette = "Greys") +
-    geom_bar(data = filter(sd_stats_top, registered == 1), 
+    geom_bar(data = filter(data, registered == 1), 
              aes(fill = year), position = position_stack(reverse = T), stat = "identity") +
-    scale_fill_brewer(palette = "YlOrBr") + 
-    ggtitle(paste(min(levels(sd_stats$year)), "-", max(levels(sd_stats$year)), sep = "")) + 
+    scale_fill_brewer(palette = p) + 
+    theme(legend.position = "bottom",
+          legend.spacing.x = unit(0.25, 'cm'),
+          legend.title = element_blank()) + 
+    ggtitle(paste("Applications to Registrations: ", min(levels(data$year)), "-", max(levels(data$year)), 
+                  "\n(total applications shown in grey)", sep = "")) + 
     theme(plot.title = element_text(hjust = 0.5)) + 
-    xlab("School District") +
-    guides(fill = guide_legend(title = "Year"))
-
+    xlab("School District")
   
 }  
 
@@ -58,6 +63,6 @@ plot_conversions_by_sd <- function(year = seq(1950,2050), palette = "Greys", dat
 
 
 # plot applications to registrations proportions 
-plot_conversions_by_sd(year = 2017:2018)
+plot_conversions_by_sd(year = 2017:2018, palette = "Reds")
 plot_conversions_by_sd(2017, palette = "YlOrBr")
 plot_conversions_by_sd(2018, palette = "BuGn")
