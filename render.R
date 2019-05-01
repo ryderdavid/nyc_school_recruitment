9aa1ffcfc861974035ad22065276e54d2808ce3b
Ryder Cobean <ryderdavid@gmail.com>
2019-05-01 02:10:09 -0400

    updates

--- render.R
+++ render.R
@@ -1,9 +1,14 @@
+library(markdown)
 library(rmarkdown)
+library(knitr)
 
 output_dir <- "./docs"
 
 source('setup.R')
 
 render("recruitment_history.Rmd", output_dir = output_dir, params = list(output_dir = output_dir))
-render("ad_targeting.Rmd", output_dir = output_dir, params = list(output_dir = output_dir))
+render("recruitment_strategies.Rmd", output_dir = output_dir, params = list(output_dir = output_dir))
+
+rpubsUpload(title = 'Bronx School Ad Placement', ./docs/school_ad_targeting.html', )
+
 
